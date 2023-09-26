6b8e80c08b1b673ee229802601d10b26ef8420f6
lanshufan <1301129162@qq.com>
2023-09-26 18:23:57 +0800

    fix responseStatus

--- controller/log_process.go
+++ controller/log_process.go
@@ -112,7 +112,7 @@ func (l *LogProcess) WriteToInfluxDB() {
 		tags["upstreamAddr"] = str.UpstreamAddr
 		tags["upstreamTime"] = strconv.FormatFloat(str.UpstreamTime, 'f', 2, 64)
 		tags["responseTime"] = strconv.FormatFloat(str.ResponseTime, 'f', 2, 64)
-		tags["responseStatus"] = string(str.ResponseStatus)
+		tags["responseStatus"] = strconv.Itoa(str.ResponseStatus)
 
 		// field字段
 		fields = make(map[string]interface{})
